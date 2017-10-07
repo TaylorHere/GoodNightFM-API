92c482e45cf3bb387f48d2e82b4bbfa3f98bf29c
taylor <taylor@huacaoxiu.com>
2017-10-08 01:10:09 +0800

    新增数据库设置

---
+++ model/Base.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import sys
+sys.path.append('..')
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///escort.db', convert_unicode=True, echo=False)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=True,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
